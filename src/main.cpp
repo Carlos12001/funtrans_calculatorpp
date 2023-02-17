1c9a779aafdcd22bbaecb81e609f1bcc6336d8de
Carlos12001 <carlos.andres12001@gmail.com>
2023-02-17 01:40:27 -0600

    The lib Boost::Multiprecision works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
 #include "gui/init_gui.h"
+#include <iostream>
+#include <iomanip>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/math/constants/constants.hpp>
+using boost::multiprecision::cpp_dec_float_50;
+
+
+template<typename T>
+inline T area_of_a_circle(T r){
+   using boost::math::constants::pi;
+   return pi<T>() * r * r;
+}
 
 int main(int argc, char *argv[]){
+    const float r_f(float(123) / 100);
+    const float a_f = area_of_a_circle(r_f);
+
+    const double r_d(double(123) / 100);
+    const double a_d = area_of_a_circle(r_d);
+
+    const cpp_dec_float_50 r_mp(cpp_dec_float_50(123) / 100);
+    const cpp_dec_float_50 a_mp = area_of_a_circle(r_mp);
+
+    // 4.75292
+    std::cout
+      << std::setprecision(std::numeric_limits<float>::digits10)
+      << a_f
+      << std::endl;
+
+    // 4.752915525616
+    std::cout
+      << std::setprecision(std::numeric_limits<double>::digits10)
+      << a_d
+      << std::endl;
+
+    // 4.7529155256159981904701331745635599135018975843146
+    std::cout
+      << std::setprecision(std::numeric_limits<cpp_dec_float_50>::digits10)
+      << a_mp
+      << std::endl;
     return InitGUI::start(argc, argv);
 }
