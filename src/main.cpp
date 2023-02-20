ceaa7945171e4ce5fbb9869b7d2a18e01b90d2d0
Carlos12001 <carlos.andres12001@gmail.com>
2023-02-19 18:26:13 -0600

    Main testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,5 @@ int main(int argc, char *argv[]){
     cout << std::fixed << funtrans::factorial_t(50).str() << endl;
     cout << std::fixed << funtrans::power_t(0, 0).str() << endl;
     cout << std::fixed << funtrans::divi_t(3).str() << endl;
-
     return 0;
 }
