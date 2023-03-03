901a486111a150022045aeaab67eb2b22929a405
Carlos12001 <carlos.andres12001@gmail.com>
2023-03-03 09:27:13 -0600

    Open the local Manual.

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.hpp"
+#include "../header.hpp"
 #include "./ui_mainwindow.h"
 #include <QTime>
 #include <Qtimer>
+#include <QDesktopServices>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow){
     ui->setupUi(this);
+    connect(ui->buttonHelp,SIGNAL(clicked()),this,SLOT(on_button_help_clicked()));
 }
 
 MainWindow::~MainWindow(){
     delete ui;
+}
+
+void MainWindow::on_button_help_clicked() {
+    cout << "Open Manual" << endl;
+    QDesktopServices::openUrl(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/resources/user_manual.pdf"));
 }
--- src/gui/mainwindow.hpp
+++ src/gui/mainwindow.hpp
@@ -17,5 +17,29 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+
+
+    void on_button_help_clicked();
+//    void on_button_clear_clicked();
+//    void on_button_divi_clicked();
+//    void on_button_exp_clicked();
+//    void on_button_sin_clicked();
+//    void on_button_cos_clicked();
+//    void on_button_tan_clicked();
+//    void on_button_ln_clicked();
+//    void on_button_logy_clicked();
+//    void on_button_power_clicked();
+//    void on_button_sinh_clicked();
+//    void on_button_cosh_clicked();
+//    void on_button_sqrt_clicked();
+//    void on_button_tanh_clicked();
+//    void on_button_root_clicked();
+//    void on_button_asin_clicked();
+//    void on_button_atan_clicked();
+//    void on_button_csc_clicked();
+//    void on_button_sec_clicked();
+//    void on_button_cot_clicked();
 };
 #endif // MAINWINDOW_H
